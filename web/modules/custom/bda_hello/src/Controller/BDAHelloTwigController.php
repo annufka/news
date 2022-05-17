41ebac2f1d2f1292b058d2f324214721dc62fc3f
didukh_a <annufka94@gmail.com>
2022-05-17 16:01:18 +0300

    Add template .twig

---
+++ web/modules/custom/bda_hello/src/Controller/BDAHelloTwigController.php
@@ -0,0 +1,16 @@
+<?php
+ 
+namespace Drupal\bda_hello\Controller;
+ 
+use Drupal\Core\Controller\ControllerBase;
+ 
+class BDAHelloTwigController extends ControllerBase {
+  public function content() {
+ 
+    return [
+      '#theme' => 'bda-hello-template',
+      '#test_var' => $this->t('Test Value'),
+    ];
+ 
+  }
+}
