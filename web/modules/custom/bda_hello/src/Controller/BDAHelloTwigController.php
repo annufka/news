3c790808cf7b711ee3de319cbd42736b143ae19e
didukh_a <annufka94@gmail.com>
2022-05-25 12:19:17 +0300

    hooks

--- web/modules/custom/bda_hello/src/Controller/BDAHelloTwigController.php
+++ web/modules/custom/bda_hello/src/Controller/BDAHelloTwigController.php
@@ -3,15 +3,30 @@
 namespace Drupal\bda_hello\Controller;
  
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\node\Entity\Node;
  
 class BDAHelloTwigController extends ControllerBase {
+  // public function content() {
+ 
+  //   return [
+  //     '#theme' => 'bda-hello-template',
+  //     '#test_var' => $this->t('Test Value'),
+  //   ];
+ 
+  // }
+
   public function content() {
- 
-    return [
-      '#theme' => 'bda-hello-template',
-      '#test_var' => $this->t('Test Value'),
-    ];
- 
+    $nodes = Node::loadMultiple();
+    $i = 0;
+    foreach ($nodes as $node) {
+      $i++;
+      if ($node == 'news') {
+        if ($node->hasField('field_category_choice')) {
+          $theme = 'bda-hello-template-bookmark-true';
+        }
+      }
+    }
+    return $theme;
   }
 
 }
